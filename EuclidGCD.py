fde4b0a3932fc23ff2d84bfb6579ba6e8a5fb9eb
rebekahorth1 <rebekah.orth@gmail.com>
2015-12-02 19:34:01 +0000

    Completed assignment

--- EuclidGCD.py
+++ EuclidGCD.py
@@ -1,4 +1,14 @@
 def gCd(x, y):
-    if ???# base case here
-        return ???
-    return gCd(?????)
+    print("X = ",x,"Y = ",y)
+    if y==0:
+        return x
+    else:    
+        return gCd(y,x%y)
+
+def main():
+    x = 252
+    y = 105
+    print("GCD of", x, ",",y,"=",gCd(x,y))
+    
+if __name__ =="__main__":
+    main()
